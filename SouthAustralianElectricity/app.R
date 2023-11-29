33ac37bf76eec0db7e112fd8d934f94c230e737b
Geoff Russell <geoffrey.russell@gmail.com>
2023-11-29 17:57:58 +1030

    Add plots comparing wind and solar rollout in SA with elsewhere and start to flesh out the text

--- SouthAustralianElectricity/app.R
+++ SouthAustralianElectricity/app.R
@@ -15,6 +15,17 @@ markdownFile<-function(filename) {
 }
 options(scipen=999)
 
+dfpop<-read_csv("populationRenamed2022.csv")
+dfkwPerCapSolar<-read_csv("kwPerCap2022.csv") %>% arrange(desc(kwPerCap)) %>% slice_head(n=20)
+dfkwPerCapWind<-read_csv("kwPerCap2022Wind.csv") %>% arrange(desc(kwPerCap)) %>% slice_head(n=20)
+write_csv(dfkwPerCapWind,"xxx.csv")
+dfsa<-tribble(
+  ~Country,~"2022",~Population,~MW,~kwPerCap,
+  "South Australia",3443,1.7e6,3442,(3442000/1.7e6)
+)
+dfkwPerCapWind<-bind_rows(dfkwPerCapWind,dfsa)
+dfkwPerCapWind <- dfkwPerCapWind %>% mutate(Group=case_when(Country=="South Australia" ~ "XX",.default = "YY"))
+write_csv(dfkwPerCapWind,"xxx.csv")
 dfpower<-read_csv("facilities.csv")
 dfdata<-read_csv("opennem-29-11-2023sa5.csv") %>% 
   rename_with(~sub('date','Time',.x)) %>% 
@@ -37,7 +48,11 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
     ),
     mainPanel(
       markdownFile("intro.txt"),
-      imageOutput("weekpng",height=400),
+      plotOutput("kwpercapsolar"),
+      markdownFile("obw.txt"),
+      plotOutput("kwpercapwind"),
+      markdownFile("ob0.txt"),
+      fluidRow(align="center",imageOutput("weekpng",height=400)),
       markdownFile("ob1.txt"),
       plotOutput("shortfall"),
       plotOutput("facilities")
@@ -45,9 +60,21 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
   )
 )
 server<-function(input,output) {
+  ptheme<-theme(plot.title=element_text(color="#008080",size=15,face="bold",family="Helvetica"))
   output$weekpng<-renderImage(list(src="WeekEnding29-11-2023.png",height=400),deleteFile=FALSE)
+  output$kwpercapsolar<-renderPlot({
+      dfkwPerCapSolar %>% ggplot() + geom_col(aes(x=reorder(Country,kwPerCap),y=kwPerCap),width=0.6,fill="steelblue1") + 
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + ptheme +
+      labs(x="",y="kilowatts per person",title="Solar: Top 20 countries by kilowatts per person")
+  })
+  output$kwpercapwind<-renderPlot({
+      dfkwPerCapWind %>% ggplot() + geom_col(aes(x=reorder(Country,kwPerCap),y=kwPerCap,fill=Group),width=0.6) + 
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + ptheme +
+      scale_fill_manual(values=c("red","steelblue1"))+guides(fill="none")+
+      labs(x="",y="kilowatts per person",title="Wind: Top 20 countries (plus SA) by kilowatts per person")
+  })
   output$shortfall<-renderPlot({
-      dfcs %>% ggplot() + geom_line(aes(x=Time,y=MW,color=Level)) + 
+      dfcs %>% ggplot() + geom_line(aes(x=Time,y=MW,color=Level)) +  ptheme +
         geom_line(aes(x=Time,y=cumshortMWh*coef))+
         labs(color="MW",title="Demand and non-renewable shortfall, Week ending November 28 2023")+
         scale_y_continuous(
@@ -57,7 +84,7 @@ server<-function(input,output) {
   })
   output$facilities=renderPlot({
     dfpower %>% group_by(Technology) %>% summarise(MW=sum(`Generator Capacity (MW)`)) %>%
-      ggplot()+geom_col(aes(x=Technology,y=MW),width=0.6,fill="blue")+coord_flip()
+      ggplot()+geom_col(aes(x=Technology,y=MW),width=0.6,fill="blue")+coord_flip() + ptheme 
   })
   
 }
