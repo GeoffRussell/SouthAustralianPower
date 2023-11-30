0c25019c95ca6132537cf57cccb0c5c2ec3b5599
Geoff Russell <geoffrey.russell@gmail.com>
2023-11-30 17:08:00 +1030

    Safe keeping commit ... Add general renewable energy plot by country for the top 20 plus more text

--- SouthAustralianElectricity/app.R
+++ SouthAustralianElectricity/app.R
@@ -18,6 +18,10 @@ options(scipen=999)
 dfpop<-read_csv("populationRenamed2022.csv")
 dfkwPerCapSolar<-read_csv("kwPerCap2022.csv") %>% arrange(desc(kwPerCap)) %>% slice_head(n=20)
 dfkwPerCapWind<-read_csv("kwPerCap2022Wind.csv") %>% arrange(desc(kwPerCap)) %>% slice_head(n=20)
+countries<-dfkwPerCapWind %>% select(Country)
+dfwsbh<-read_csv("wsbh.csv") %>% inner_join(countries)
+dfwsbh<-dfwsbh %>% group_by(Country) %>% summarise(sum=sum(kWhPerCap)) %>% ungroup() %>% inner_join(dfwsbh)
+
 dfsa<-tribble(
   ~Country,~"2022",~Population,~MW,~kwPerCap,
   "South Australia",3443,1.7e6,3442,(3442000/1.7e6)
@@ -29,7 +33,6 @@ dfdata<-read_csv("opennem-30-11-2023sa5.csv") %>%
   rename_with(~sub('date','Time',.x)) %>% 
   rename_with(~sub('  ',' ',.x))
 dfout<-dfdata %>% mutate(demand=select(.,`Battery (Charging) - MW`:`Solar (Rooftop) - MW`) %>% apply(1,sum)) 
-#dfoutshort<-dfout %>% mutate(short=demand-(`Solar (Utility) - MW`+`Solar (Rooftop) - MW`+`Wind - MW`),evreq=short*1e6/50e3) 
 dfcumshort<-dfout %>% mutate(short=demand-(`Solar (Utility) - MW`+`Solar (Rooftop) - MW`+`Wind - MW`),cumshortMWh=cumsum(short*5/60)) %>%
   select(Time,demand,short,cumshortMWh)
 dfcs<-dfcumshort %>% pivot_longer(cols=c("demand","short"),names_to="Level",values_to="MW") 
@@ -38,18 +41,15 @@ coef<-3/28
 
 ui <- fluidPage(theme = shinytheme("cerulean"),
   # Application title
-  titlePanel("A week in the electricity supply of South Australia"),
-  sidebarLayout(
-    sidebarPanel(
-      
-      
-    ),
+  titlePanel("South Australia's excellent renewable adventure"),
+  verticalLayout(
     mainPanel(
       markdownFile("intro.txt"),
       markdownFile("obw.txt"),
       plotOutput("kwpercapwind"),
       markdownFile("obs.txt"),
       plotOutput("kwpercapsolar"),
+      plotOutput("wsbh"),
       markdownFile("ob0.txt"),
       fluidRow(align="center",imageOutput("weekpng",height=400)),
       markdownFile("ob1.txt"),
@@ -59,12 +59,20 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
   )
 )
 server<-function(input,output) {
-  ptheme<-theme(plot.title=element_text(color="#008080",size=15,face="bold",family="Helvetica"))
+  ptheme<-theme(plot.title=element_text(color="#008080",size=15,face="bold",family="Helvetica"),
+                axis.text=element_text(face="bold",size=12)
+                )
   output$weekpng<-renderImage(list(src="WeekEnding30-11-2023.png",height=400),deleteFile=FALSE)
   output$kwpercapsolar<-renderPlot({
       dfkwPerCapSolar %>% ggplot() + geom_col(aes(x=reorder(Country,kwPerCap),y=kwPerCap),width=0.6,fill="yellow") + 
       theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + ptheme +
       labs(x="",y="kilowatts per person",title="Solar: Top 20 countries by kilowatts per person")
+  })
+  output$wsbh<-renderPlot({
+    dfwsbh %>% ggplot() + geom_col(aes(x=reorder(Country,desc(sum)),y=kWhPerCap,fill=Type)) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,color=c(rep("black",4),"red",rep("black",15)))) + ptheme +
+      labs(x="",y="Annual kilowatt-hours per person",
+           title="Wind/Solar/Biomass/Hydro: Top 20 \ncountries by wind capacity")
   })
   output$kwpercapwind<-renderPlot({
       dfkwPerCapWind %>% ggplot() + geom_col(aes(x=reorder(Country,kwPerCap),y=kwPerCap,fill=Group),width=0.6) + 
